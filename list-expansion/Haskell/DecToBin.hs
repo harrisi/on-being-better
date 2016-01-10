43f12e9365c4d122e93d69554d17e6c7d35ddd18
Ian Harris <ian+gh@dgby.org>
2016-01-10 00:51:07 -0800

    Added Haskell module for dec->bin
    DecToBin.hs is a simple module for converting decimal to binary data.

---
+++ list-expansion/Haskell/DecToBin.hs
@@ -0,0 +1,9 @@
+module DecToBin where
+
+decToBin':: Int -> [Int] -> [Int]
+decToBin' n b
+  | n `div` 2 == 0 = reverse $ n `mod` 2 : b
+  | otherwise = n `mod` 2 : decToBin' (n `div` 2) b
+
+decToBin :: Int -> String
+decToBin n = concat . map show . reverse $ decToBin' n []
